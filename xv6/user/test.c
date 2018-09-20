3fb58250487a990154ed10f3d078c61b40d069b8
francisco <francisco.miranda.92@gmail.com>
2018-09-20 14:44:53 -0500

    added XV6 with readcount() implemented

---
+++ xv6/user/test.c
@@ -0,0 +1,8 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void){
+  printf(1,"The number of times read() has been implemented is %d\n", getreadcount());
+    exit();
+    }
